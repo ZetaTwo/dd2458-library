090b1e05c53d278db12dc52e81a6617c5467ae6a
Calle Svensson <calle.svensson@zeta-two.com>
2014-09-05 14:48:19 +0200

    Fenwick template.

--- labb1/labb1-library/fenwick.hpp
+++ labb1/labb1-library/fenwick.hpp
@@ -3,34 +3,34 @@
 #include <vector>
 using std::vector;
 
-class FenwickTree
-{
+template<typename T>
+class FenwickTree {
 public:
-  FenwickTree(int N) {
-    tree.resize(N);
+  FenwickTree(int size) {
+    tree.resize(size);
   }
 
-  // Increase index 'i' by 'delta'
-  void add(int i, int delta) {
-    for (; i < tree.size(); i |= i+1)
+  void add(int i, T delta) {
+    for (; i < (int)tree.size(); i |= i + 1)
       tree[i] += delta;
   }
 
-  // Returns sum of elements with from index 'left' to 'right'
-  int getsum(int left, int right) {
-    return sum(right) - sum(left-1);
+  T sum(int left, int right) {
+    return sum(right) - sum(left - 1);
   }
 
 private:
-  vector<int> tree;
+  vector<T> tree;
 
-  int sum(int index) {
-    int sum = 0;
+  T sum(int index) {
+    T sum = 0;
+
     while (index >= 0) {
       sum += tree[index];
       index &= index + 1;
       index--;
     }
+
     return sum;
   }
 };
--- labb1/labb1-tests/fenwick-tests.cpp
+++ labb1/labb1-tests/fenwick-tests.cpp
@@ -3,23 +3,23 @@
 #include "../labb1-library/fenwick.hpp"
 
 TEST(FenwickTree, Test1) {
-  FenwickTree tree1(4);
+  FenwickTree<int> tree1(4);
   tree1.add(0, 10);
   tree1.add(1, 10);
   tree1.add(2, 10);
   tree1.add(3, 10);
 
-  EXPECT_EQ(10, tree1.getsum(0, 0));
-  EXPECT_EQ(10, tree1.getsum(1, 1));
-  EXPECT_EQ(10, tree1.getsum(2, 2));
-  EXPECT_EQ(10, tree1.getsum(3, 3));
+  EXPECT_EQ(10, tree1.sum(0, 0));
+  EXPECT_EQ(10, tree1.sum(1, 1));
+  EXPECT_EQ(10, tree1.sum(2, 2));
+  EXPECT_EQ(10, tree1.sum(3, 3));
 
-  EXPECT_EQ(20, tree1.getsum(0, 1));
-  EXPECT_EQ(20, tree1.getsum(1, 2));
-  EXPECT_EQ(20, tree1.getsum(2, 3));
+  EXPECT_EQ(20, tree1.sum(0, 1));
+  EXPECT_EQ(20, tree1.sum(1, 2));
+  EXPECT_EQ(20, tree1.sum(2, 3));
 
-  EXPECT_EQ(30, tree1.getsum(0, 2));
-  EXPECT_EQ(30, tree1.getsum(1, 3));
+  EXPECT_EQ(30, tree1.sum(0, 2));
+  EXPECT_EQ(30, tree1.sum(1, 3));
 
-  EXPECT_EQ(40, tree1.getsum(0, 3));
+  EXPECT_EQ(40, tree1.sum(0, 3));
 }
