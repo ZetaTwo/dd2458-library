3a3df51176bf04d93911be012b2f756c1531d760
Calle Svensson <calle.svensson@zeta-two.com>
2014-09-06 21:30:45 +0200

    Cleaned up knapsack. Still not fast enough.

--- labb1/labb1-knapsack-runner/knapsack.cpp
+++ labb1/labb1-knapsack-runner/knapsack.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
+#include <cstdio>
+#include <vector>
 using namespace std;
 
 #include "knapsack.hpp"
 using namespace knapsack;
+
+#ifdef _WIN32
+#define scanf scanf_s
+#endif
 
 int main() {
   //Input parameters
@@ -10,22 +15,23 @@ int main() {
   int n;
 
   //Continue while there are more test cases
-  while (cin >> C >> n) {
+  while (scanf("%lf %d\n", &C, &n) == 2) {
     //Read items
     vector<knapsack::item> items;
-    for (size_t i = 0; i < n; i++)
+    for (size_t i = 0; i < (size_t)n; i++)
     {
       value_type value;
       weight_type weight;
-      cin >> value >> weight;
+      //cin >> value >> weight;
+      scanf("%d %d\n", &value, &weight);
       items.push_back({ value, weight });
     }
 
     //Perform dynamic programming knapsack
-    vector<size_t> knapsack_indices = knapsack::knapsack(C, items);
+    vector<size_t> knapsack_indices = knapsack::knapsack(static_cast<weight_type>(C), items);
 
     //Output result
-    cout << knapsack_indices.size() << endl;
+    printf("%d\n", knapsack_indices.size());
     if (knapsack_indices.size() > 0) {
       bool first = true;
       for (vector<size_t>::const_iterator itr = knapsack_indices.begin(); itr != knapsack_indices.end(); itr++)
@@ -34,11 +40,11 @@ int main() {
           first = false;
         }
         else {
-          cout << " ";
+          printf(" ");
         }
-        cout << *itr;
+        printf("%d", *itr);
       }
-      cout << endl;
+      printf("\n");
     }
   }
 
--- labb1/labb1-library/knapsack.hpp
+++ labb1/labb1-library/knapsack.hpp
@@ -11,44 +11,30 @@ namespace knapsack {
   };
 
   std::vector<size_t> knapsack(weight_type capacity, const std::vector<item>& items) {
-    std::vector<std::vector<value_type> > knapsack(items.size(), vector<int>(capacity+1, 0));
+    std::vector<std::vector<value_type> > knapsack(items.size()+1, vector<int>(capacity+1, 0));
 
     //Calculate dynprog table
-    for (size_t c = 1; c <= capacity; c++)
+    for (weight_type c = 1; c <= capacity; c++)
     {
-      for (size_t i = 0; i < items.size(); i++) {
-        //Multiple items branch
-        if (i > 0) {
-          knapsack[i][c] = knapsack[i - 1][c];
-          if (items[i].weight <= c) {
-            value_type candidate_value = knapsack[i - 1][c - items[i].weight] + items[i].value;
-            if (candidate_value > knapsack[i][c]) {
-              knapsack[i][c] = candidate_value;
-            }
-          }
-        }
-        //Base values branch
-        else {
-          knapsack[i][c] = 0;
-          if (items[i].weight <= c) {
-            knapsack[i][c] = items[i].value;
+      for (size_t i = 1; i <= items.size(); i++) {
+        knapsack[i][c] = knapsack[i - 1][c];
+        if (items[i-1].weight <= c) {
+          value_type candidate_value = knapsack[i - 1][c - items[i-1].weight] + items[i-1].value;
+          if (candidate_value > knapsack[i][c]) {
+            knapsack[i][c] = candidate_value;
           }
         }
       }
     }
 
     //Backtrack which items used
-    weight_type it = items.size() - 1;
     weight_type ca = capacity;
     vector<size_t> result;
-    while (ca > 0 && it >= 0)
+    for (int i = items.size()-1; i >= 0 && ca >= 0; i--)
     {
-      if (ca - items[it].weight >= 0 && knapsack[it][ca] - items[it].value == knapsack[it][ca - items[it].weight]) {
-        result.push_back(it);
-        ca -= items[it].weight;
-      }
-      else {
-        it--;
+      if (ca - items[i].weight >= 0 && knapsack[i+1][ca] - items[i].value == knapsack[i+1][ca - items[i].weight]) {
+        result.push_back(i);
+        ca -= items[i].weight;
       }
     }
     std::reverse(result.begin(), result.end());
