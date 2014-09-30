bc4fc226b82762d0a6365734e251a1f1c0d19cf2
mangefox <fox@fojji.se>
2014-09-30 21:31:58 +0200

    Some comments in longincsubseq

--- labb1/labb1-library/longincsubseq.hpp
+++ labb1/labb1-library/longincsubseq.hpp
@@ -15,6 +15,8 @@ std::vector<int> longest_seq(vector<int>& list)
 
   indices.push_back(0);
 
+  // If next element list[i] is greater than last element of
+  // current longest subsequence list[indices.back()], just push it at back of "indices" and continue
   for (size_t i = 1; i < list.size(); i++) {
     if (list[indices.back()] < list[i]) {
       p[i] = indices.back();
@@ -22,8 +24,11 @@ std::vector<int> longest_seq(vector<int>& list)
       continue;
     }
 
+    // Binary search to find the smallest element referenced by b which is just bigger than list[i]
+    // Note : Binary search is performed on indices (and not list).
+    // Size of indeces is always <=k and hence contributes O(log k) to complexity.
     int low = 0;
-    int hi = indices.size() - 1;
+    int hi = indices.size()-1;
     while (low < hi) {
       int mid = (low + hi) / 2;
       if (list[indices[mid]] < list[i])
@@ -32,15 +37,16 @@ std::vector<int> longest_seq(vector<int>& list)
         hi = mid;
     }
 
+    // Update indeces if new value is smaller than previously referenced value
     if (list[i] < list[indices[low]]) {
       if (low > 0)
-        p[i] = indices[low - 1];
+        p[i] = indices[low-1];
       indices[low] = i;
     }
   }
 
   int j = indices.back();
-  for (int i = indices.size() - 1; i >= 0; i--) {
+  for (int i = indices.size()-1; i >= 0; i--) {
     indices[i] = j;
     j = p[j];
   }
