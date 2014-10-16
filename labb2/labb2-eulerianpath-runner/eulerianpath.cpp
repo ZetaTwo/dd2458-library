e7fb4ff4c105b71e502855e165886d4323feede5
Calle Svensson <calle.svensson@zeta-two.com>
2014-10-16 23:33:46 +0200

    Started Eulerian path

---
+++ labb2/labb2-eulerianpath-runner/eulerianpath.cpp
@@ -0,0 +1,30 @@
+// KTH DD2458 popuph14
+// authors: magolsso@kth.se
+//          carlsven@kth.se
+
+// http://en.wikipedia.org/wiki/Eulerian_path#Hierholzer.27s_algorithm
+
+#include <iostream>
+using namespace std;
+
+#include "eulerianpath.hpp"
+
+int main() {
+  int N, M;
+  while (cin >> N >> M) {
+    if (N == 0 && M == 0) {
+      break;
+    }
+
+    Graph graph(N);
+    for (int m = 0; m < M; m++)
+    {
+      int u, v;
+      cin >> u >> v;
+      graph.add_edge(u, v);
+    }
+
+  }
+
+  return 0;
+}
---
+++ labb2/labb2-library/eulerianpath.hpp
@@ -0,0 +1,6 @@
+// KTH DD2458 popuph14
+// authors: magolsso@kth.se
+//          carlsven@kth.se
+#pragma once
+
+#include "graph.hpp"
