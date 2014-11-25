46b6c37589033a670d5c9fd4191952799540fab8
Calle Svensson <calle.svensson@zeta-two.com>
2014-11-25 03:30:26 +0100

    Almost working point-in-polygon

---
+++ labb4/labb4-pointinpolygon-runner/pointinpolygon.cpp
@@ -0,0 +1,47 @@
+// KTH DD2458 popuph14
+// authors: magolsso@kth.se
+//          carlsven@kth.se
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+#include "point.hpp"
+
+int main() {
+  size_t n;
+  while (cin >> n) {
+    if (n == 0) break;
+
+    vector<point<long long> > polygon(n);
+    for (size_t i = 0; i < n; i++) {
+      cin >> polygon[i];
+    }
+
+    size_t m;
+    cin >> m;
+    while (m--) {
+      point<long long> p;
+      cin >> p;
+      switch (inside_poly(p, polygon.begin(), polygon.end())) {
+      case 0:
+        cout << "out";
+        break;
+      case 1:
+        cout << "on";
+        break;
+      case 2:
+        cout << "in";
+        break;
+      default:
+        break;
+      }
+      cout << endl;
+    }
+  }
+
+
+  return 0;
+}
